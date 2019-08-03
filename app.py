de3b57d306d1c2733efb2094cb962d4728d5e4da
Enderson Vizcaino <enjaviga@gmail.com>
2019-08-02 20:31:41 -0500

    Update API :ok_hand:

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import pafy
 import json
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*":{"origins":"https://edy-pro.herokuapp.com/*"}})
+CORS(app, resources={r"/api/*":{"origins":"*"}})
 
 @app.route("/api/<string:id>/blob", methods=['GET'])
 def apiedyblob(id):
+    # video = asyncio.coroutine(pafy.new(id));
+    # print(video)
+
     video = pafy.new(id)
+    print(video)
+
     data = {
         'title': video.title,
         'video': []
@@ -24,38 +29,39 @@ def apiedyblob(id):
 
 @app.route("/api/<string:url>", methods=['GET'])
 def apiedy(url):
-    video = pafy.new(url)
-    print(video)
-    data = {'data':
-            {
-                'titulo': video.title,
-                'imagen': video.thumb,
-                'duracion': video.duration,
-                'videostream': [],
-                'audiostream': [],
-            }
-            }
+    try:
+        video = pafy.new(url)
+        data = {'data':
+                {
+                    'titulo': video.title,
+                    'imagen': video.bigthumbhd,
+                    'duracion': video.duration,
+                    'videostream': [],
+                    'audiostream': [],
+                }
+                }
+        for stream in video.streams:
+            data['data']['videostream'].append(
+                {
+                    'resolucion': str(stream.resolution),
+                    'extencion': str(stream.extension),
+                    'tamano': str(stream.get_filesize()),
+                    'url': str(stream.url),
+                })
 
-    for stream in video.streams:
-        data['data']['videostream'].append(
-            {
-                'resolucion': str(stream.resolution),
-                'extencion': str(stream.extension),
-                'tamano': str(stream.get_filesize()),
-                'url': str(stream.url),
-            })
+        for audiostream in video.audiostreams:
+            data['data']['audiostream'].append(
+                {
+                    'bitrate': str(audiostream.bitrate),
+                    'extencion': str(audiostream.extension),
+                    'tamano': str(audiostream.get_filesize()),
+                    'url': str(audiostream.url)
+                })
 
-    for audiostream in video.audiostreams:
-        data['data']['audiostream'].append(
-            {
-                'bitrate': str(audiostream.bitrate),
-                'extencion': str(audiostream.extension),
-                'tamano': str(audiostream.get_filesize()),
-                'url': str(audiostream.url)
-            })
-
-    return jsonify(data)
-
+        return jsonify(data)
+    except:
+        return jsonify({'data': None, 'error': "An error occured" })        
+    
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
